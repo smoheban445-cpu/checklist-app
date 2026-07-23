ff41f8abe51cfbe8003a57cdc524deca28777670
smoheban445-cpu <smoheban445@gmail.com>
2026-07-23 23:33:41 +0330

    Add files via upload
    دستیار نسخه اندروید

---
+++ sw.js
@@ -0,0 +1,58 @@
+﻿const CACHE_NAME = 'checklist-v1';
+const urlsToCache = [
+  '/checklist-app/',
+  '/checklist-app/index.html',
+  '/checklist-app/manifest.json',
+  '/checklist-app/icon-192.png',
+  '/checklist-app/icon-512.png'
+];
+
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(cache => {
+        console.log('کش باز شد');
+        return cache.addAll(urlsToCache);
+      })
+      .then(() => self.skipWaiting())
+  );
+});
+
+self.addEventListener('activate', event => {
+  event.waitUntil(
+    caches.keys().then(cacheNames => {
+      return Promise.all(
+        cacheNames.map(cacheName => {
+          if (cacheName !== CACHE_NAME) {
+            console.log('حذف کش قدیمی:', cacheName);
+            return caches.delete(cacheName);
+          }
+        })
+      );
+    }).then(() => self.clients.claim())
+  );
+});
+
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request)
+      .then(response => {
+        if (response) {
+          return response;
+        }
+        return fetch(event.request).then(
+          response => {
+            if (!response || response.status !== 200 || response.type !== 'basic') {
+              return response;
+            }
+            const responseToCache = response.clone();
+            caches.open(CACHE_NAME)
+              .then(cache => {
+                cache.put(event.request, responseToCache);
+              });
+            return response;
+          }
+        );
+      })
+  );
+});
